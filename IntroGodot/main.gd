dc74e721a2c740fd4637f7e5ae8b3d6dc95a990e
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2024-04-09 18:38:50 -0300

    Intro Godot - Label Updates every Second

--- IntroGodot/main.gd
+++ IntroGodot/main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-var total: int = 0
+var total: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,7 +9,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	total += 1
+	total += delta
 	update_score(total)
 
 func update_score(score: float) -> void:
