df3b3345e76c23bd29c74784c3e798bc6779c526
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2024-04-09 18:50:15 -0300

    Intro Godot - Polling

--- IntroGodot/main.gd
+++ IntroGodot/main.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+const SPEED : int = 100
+
 var total: float = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -11,8 +13,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	total += delta
 	update_score(total)
+	
+	# Used for continuous actions (movement)
+	if Input.is_action_pressed("ui_left"):
+		position.x -= SPEED * delta # Pixels/segundo
+	elif Input.is_action_pressed("ui_right"):
+		position.x += SPEED * delta
 
 func _input(event: InputEvent) -> void:
+	# Used for single presses, such as "ESC" for menu, for example
 	if event.is_action_pressed("ui_right"):
 		print("Right Arrow!\n")
 
