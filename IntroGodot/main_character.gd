f413e012ca5ef54006a71dd9de091cd1d28786cd
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2024-04-11 20:34:57 -0300

    Intro Godot - New Scene
    Moved main character
    New Input Events

--- IntroGodot/main_character.gd
+++ IntroGodot/main_character.gd
@@ -3,28 +3,25 @@ extends CharacterBody2D
 @export var speed: int = 400
 @onready var _animated_sprite = $AnimatedSprite
 
-const directions = ["left", "right", "up", "down"]
+const directions = ["move_left", "move_right", "move_up", "move_down"]
+const LEFT = directions[0]
+const RIGHT = directions[1]
+const UP = directions[2]
+const DOWN = directions[3]
 
-func get_input() -> void:
-	const LEFT = "move_" + directions[0]
-	const RIGHT = "move_" + directions[1]
-	const UP = "move_" + directions[2]
-	const DOWN = "move_" + directions[3]
+func get_8way_input() -> void:
 	var input_direction: Vector2 = Input.get_vector(LEFT, RIGHT, UP, DOWN)
 	velocity = input_direction * speed
+	#print(velocity)
 
 func _physics_process(delta) -> void:
-	get_input()
+	#1. 8 Way movement
+	get_8way_input()
 	move_and_slide()
 
 func _process(delta) -> void:
-	var move: String
-	var previousMove: String
 	for direction in directions:
-		move = "move_" + direction
-		if move != previousMove:
-			previousMove = move
-			if Input.is_action_pressed(move):
-				_animated_sprite.play(move)
-			else:
-				_animated_sprite.stop()
+		if Input.is_action_pressed(direction):
+			_animated_sprite.play(direction)
+		else:
+			_animated_sprite.stop()
