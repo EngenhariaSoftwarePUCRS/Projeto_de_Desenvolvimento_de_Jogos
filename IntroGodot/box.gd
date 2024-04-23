6b61c51c24658ba71387bd292912ca8f2e12a816
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2024-04-23 19:23:06 -0300

    Intro Godot - Tween Animatino

--- IntroGodot/box.gd
+++ IntroGodot/box.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+func _ready() -> void:
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
+	#tween.tween_property(self, "scale", Vector2(0.688, 0.703), 0.2)
+	#tween.tween_property(self, "scale", Vector2(0.313, 0.313), 0.2)
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	print("Saiu!")
