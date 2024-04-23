c05a2d60a5e49e07f40dce8779fcd83f9679700d
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2024-04-23 18:34:34 -0300

    Intro Godot - Change Level / Death

--- IntroGodot/game.gd
+++ IntroGodot/game.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var hud : CanvasLayer = $HUD
 
 var gameScore : int = 0
+var currentScene = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,8 +14,13 @@ func _ready() -> void:
 	#player.connect("jumped", _on_jumped)
 
 func _physics_process(delta: float) -> void:
-	if player.position.y < sceneLimit.position.y:
+	if sceneLimit == null:
+		player = $Level/MainCharacter
+		sceneLimit = $Level/SceneLimit
+	if player.position.y > sceneLimit.position.y:
 		get_tree().change_scene_to_file("res://game_over.tscn")
+	if Input.is_action_just_pressed("change"):
+		call_deferred("goto_scene", "res://level2.tscn")
 
 func updateScore():
 	gameScore += 1
@@ -22,3 +28,12 @@ func updateScore():
 
 func _on_jumped():
 	updateScore()
+
+func goto_scene(path: String):
+		print("Total children: " + str(get_child_count))
+		var world := get_child(1)
+		world.free()
+		var res := ResourceLoader.load(path)
+		currentScene = res.instantiate()
+		sceneLimit = null
+		add_child(currentScene)
