0208368c06058e7abb78fc5163d4170f4dcf5103
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-06-09 23:39:57 -0300

    Save

--- FugaDosPets/scripts/levels/level_1.gd
+++ FugaDosPets/scripts/levels/level_1.gd
@@ -1,8 +1,34 @@
 extends Node
 
 
+@onready var camera_a: Camera2D = $CameraA
+@onready var camera_b: Camera2D = $CameraB
+@onready var camera_c: Camera2D = $CameraC
+@onready var camera_d: Camera2D = $CameraD
 @onready var character: CharacterBody2D = $Character
+@onready var checkpoint_a: Area2D = $Scenery/CheckpointA
+@onready var checkpoint_b: Area2D = $Scenery/CheckpointB
+@onready var checkpoint_c: Area2D = $Scenery/CheckpointC
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	print("Loaded Level 1")
+	set_active_camera(camera_a)
+
+
+func set_active_camera(camera: Camera2D) -> void:
+	camera.make_current()
+
+
+func _on_checkpoint_a_body_entered(body: Node2D) -> void:
+	checkpoint_a.queue_free()
+	set_active_camera(camera_b)
+
+
+func _on_checkpoint_b_body_entered(body: Node2D) -> void:
+	checkpoint_b.queue_free()
+	set_active_camera(camera_c)
+
+
+func _on_checkpoint_c_body_entered(body: Node2D) -> void:
+	checkpoint_c.queue_free()
+	set_active_camera(camera_d)
