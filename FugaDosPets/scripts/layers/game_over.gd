74f412eebe3f8b7b820416514bdccb2c19c031de
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-07-07 15:18:27 -0300

    fix: Game Over Screen Typing

--- FugaDosPets/scripts/layers/game_over.gd
+++ FugaDosPets/scripts/layers/game_over.gd
@@ -1,17 +1,17 @@
 extends Control
 
 
-@onready var play_again = $PlayAgain
+@onready var play_again: Button = $PlayAgain
 
 
-func _on_settings_btn_pressed():
+func _on_settings_btn_pressed() -> void:
 	get_tree().call_group("main", "show_settings")
 
 
-func _on_play_again_pressed():
+func _on_play_again_pressed() -> void:
 	get_tree().call_group("main", "restart_level")
 
 
-func _on_exit_pressed():
+func _on_exit_pressed() -> void:
 	get_tree().call_group("main", "return_to_home")
 
