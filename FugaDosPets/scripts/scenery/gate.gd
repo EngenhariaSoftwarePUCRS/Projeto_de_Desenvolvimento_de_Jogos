91c7fdbb740935c5b5cb663e8b704343576fa265
Felipe Freitas Silva <ffreitas@pucrs.br>
2024-07-08 11:46:39 -0300

    Gate Texture

--- FugaDosPets/scripts/scenery/gate.gd
+++ FugaDosPets/scripts/scenery/gate.gd
@@ -2,17 +2,20 @@ extends StaticBody2D
 
 
 @export var opened: bool
-
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var closed_sprite = $ClosedSprite
+@onready var opened_sprite = $OpenedSprite
 @onready var collider: CollisionShape2D = $CollisionShape2D
 
 
 func _ready() -> void:
 	opened = false
+	closed_sprite.visible = false
 
 
 func open() -> void:
 	if opened:
 		return
 	opened = true
+	closed_sprite.visible = true
+	opened_sprite.visible = false
 	collider.set_deferred("disabled", true)
